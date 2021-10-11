b8859d84adb9784081b3fd5678990842bf5566f3
Alex Rakotoarisolo <salerafiot16@gmail.com>
2021-10-12 00:13:32 +0200

    Updated by Alex

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+
         System.out.print("Hello, my name is Lleir ");
+        System.out.println("\nHello Lleir, my name is Alex");
     }
 }
